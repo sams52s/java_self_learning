49fca7520fa6bdf6063320e8d19149dd7607bd2a
sams52s <sams52tas@gmail.com>
2022-03-23 16:01:20 +0600

    first push

---
+++ Basic java/ResultProcessing/src/dsi/sams/Handler/fileHandler.java
@@ -0,0 +1,23 @@
+package dsi.sams.Handler;
+
+public class fileHandler {
+
+    /**
+     * This method will read my all file.
+     * @param filePath the file path I want read.
+     * It will return all values as list
+     */
+    public void FileReader(String filePath){
+        //..
+    }
+
+    /**
+     * This method writes two csv file markSheet and result file.
+     * @param fileName is for naming that csv file
+     * This method will take a listed parameter and use those to write it.
+     */
+    public void FileWriter(String fileName/* Listed parameter */){
+        //..
+    }
+
+}
---
+++ Basic java/ResultProcessing/src/dsi/sams/Handler/resultHandler.java
@@ -0,0 +1,20 @@
+package dsi.sams.Handler;
+
+public class resultHandler {
+    fileHandler fileUHandler=new fileHandler();
+
+    /**
+     * this method will read all those files
+     * then store all file information in a list and break it down in its necessary part
+     * after that It will create a list for output
+     * then it sends that list to file Writer to write my desire file.
+     */
+    public void resultGenerator(){
+        fileUHandler.FileReader("");
+        /**
+         *
+         *
+         */
+        fileUHandler.FileWriter("");
+    }
+}
